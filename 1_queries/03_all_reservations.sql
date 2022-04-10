9b4c2ae9137b6f9710a2b9a702151deda7b5bbfe
hseiji <henrique.hst@gmail.com>
2022-04-10 10:55:37 -0400

    complete 1_queries folder

---
+++ 1_queries/03_all_reservations.sql
@@ -0,0 +1,18 @@
+-- Select the reservation id, property title, reservation start_date, property cost_per_night and the average rating of the property. You'll need data from both the reservations and properties tables.
+-- The reservations will be for a single user, so just use 1 for the user_id.
+-- Order the results from the earliest start_date to the most recent start_date.
+-- Limit the results to 10.
+
+SELECT reservations.id, properties.title, start_date,cost_per_night, AVG(rating) as average_rating
+FROM reservations
+JOIN properties ON properties.id = property_id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 6
+GROUP BY reservations.id, properties.title, cost_per_night
+ORDER BY start_date
+LIMIT 10;
+
+
+-- select *
+-- from users
+-- where email = 'tristanjacobs@gmail.com';
